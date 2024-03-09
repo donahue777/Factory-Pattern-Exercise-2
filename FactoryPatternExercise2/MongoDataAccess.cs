2d52a1d307ccdac8e1bff39142aa4242064a264b
Thomas Donahue <donahue777@yahoo.com>
2024-03-09 07:45:34 -0500

    exercise 2 done

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){Name = "Pen", Price = 100},
+            new Product (){Name = "Ferrari", Price = 1000000},
+            new Product (){Name = "Mansion", Price = 7000000}
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I'm reading Mongo data.");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I've saved data for my Mongo database.");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input;
+            bool correctInput;
+            do
+            {
+                correctInput = true;
+                Console.WriteLine("What database do you want to use?");
+                Console.WriteLine("list, sql, or mongo?");
+                input = Console.ReadLine();
+
+                if (input != "list" && input != "sql" && input != "mongo")
+                {
+                    correctInput = false;
+                }
+            } while (correctInput == false);
+
+            IDataAccess database = DataAccessFactory.GetDataAccessType(input);
+
+            var products = database.LoadData();
+            database.SaveData();
+
+            foreach ( var product in products )
+            {
+                Console.WriteLine($"Product is {product.Name}, which costs {product.Price} dollars.");
+            }
         }
     }
 }
